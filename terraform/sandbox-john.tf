7d7624768722bec367c190ffef2c0692a7a3c0f0
bvaldes-k9 <bryanvaldes@hotmail.com>
2025-01-21 14:51:14 -0500

    test multi main account file john sandbox

---
+++ terraform/sandbox-john.tf
@@ -0,0 +1,27 @@
+module "sandbox-john" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "johnsandbox25491+bryanvaldes@hotmail.com"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "cursedhoundcreates@hotmail.com"
+    SSOUserFirstName          = "Bryan"
+    SSOUserLastName           = "Valdes"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
