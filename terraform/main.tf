d0fd6329f8480563dc7d16b3a0710d84f3d86153
bvaldes-k9 <bryanvaldes@hotmail.com>
2025-01-21 15:27:07 -0500

    removing sandbox sarah account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,31 +25,3 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
-
-module "dev" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "sarah25491+bryanvaldes@hotmail.com"
-    AccountName               = "sandbox-aft-sarah"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "cursedhoundcreates@hotmail.com"
-    SSOUserFirstName          = "Bryan"
-    SSOUserLastName           = "Valdes"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
